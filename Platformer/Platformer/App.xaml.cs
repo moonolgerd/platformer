ae5b50e2bfb637ba23fe904a6cb031302761556d
Oleg Merkulov <moonolgerd@gmail.com>
2017-12-06 21:52:02 -0500

    Trying out

--- Platformer/Platformer/App.xaml.cs
+++ Platformer/Platformer/App.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using Platformer.Views;
 using Xamarin.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace Platformer
 {
@@ -10,6 +13,14 @@ namespace Platformer
             InitializeComponent();
 
             MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android=f6bc5a20-8654-41c4-b424-3dc689594aa8;" + "uwp={Your UWP App secret here};" +
+                   "ios={Your iOS App secret here}",
+                   typeof(Analytics), typeof(Crashes));
+            base.OnStart();
         }
     }
 }
