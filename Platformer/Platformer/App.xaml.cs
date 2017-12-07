a1465ee5ea04129d174fff1969f7507691e1042c
Oleg Merkulov <moonolgerd@gmail.com>
2017-12-06 22:23:13 -0500

    Added iOS key for AppCenter

--- Platformer/Platformer/App.xaml.cs
+++ Platformer/Platformer/App.xaml.cs
@@ -18,7 +18,7 @@ namespace Platformer
         protected override void OnStart()
         {
             AppCenter.Start("android=f6bc5a20-8654-41c4-b424-3dc689594aa8;" + "uwp={Your UWP App secret here};" +
-                   "ios={Your iOS App secret here}",
+                   "ios=55099f39-256e-4196-b8eb-5873ee91e95e;",
                    typeof(Analytics), typeof(Crashes));
             base.OnStart();
         }
