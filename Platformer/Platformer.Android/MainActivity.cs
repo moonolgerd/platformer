37e36d9d55770092805bcce7aaf9f0276bfe4e4e
Oleg <moonolgerd@gmail.com>
2018-01-17 23:06:48 -0500

    Added Google Push Notifications

--- Platformer/Platformer.Android/MainActivity.cs
+++ Platformer/Platformer.Android/MainActivity.cs
@@ -1,18 +1,18 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Android.Gms.Common;
+using Android.Util;
+using Firebase.Messaging;
 
 namespace Platformer.Droid
 {
     [Activity(Label = "Platformer.Android", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        const string TAG = "MainActivity";
+
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -23,11 +23,43 @@ namespace Platformer.Droid
             global::Xamarin.Forms.Forms.Init(this, bundle);
 
             LoadApplication(new App());
+
+            FirebaseMessaging.Instance.SubscribeToTopic("news");
+
+            if (Intent.Extras != null)
+            {
+                foreach (var key in Intent.Extras.KeySet())
+                {
+                    var value = Intent.Extras.GetString(key);
+                    Log.Debug(TAG, "Key: {0} Value: {1}", key, value);
+                }
+            }
+
+            IsPlayServicesAvailable();
         }
 
-        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        public bool IsPlayServicesAvailable()
         {
-            base.OnActivityResult(requestCode, resultCode, data);
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    System.Diagnostics.Debug.WriteLine(GoogleApiAvailability.Instance.GetErrorString(resultCode));
+                else
+                {
+                    System.Diagnostics.Debug.WriteLine("This device is not supported");
+                    Finish();
+                }
+                return false;
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Google Play Services is available.");
+                return true;
+            }
         }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data) 
+            => base.OnActivityResult(requestCode, resultCode, data);
     }
 }
