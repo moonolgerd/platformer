344dd9f0965f324665081657d4240f4b16892efc
Oleg Merkulov <omerkulov@liquidnet.com>
2018-03-12 17:01:40 -0400

    Testing app center push

--- Platformer/Platformer/App.xaml.cs
+++ Platformer/Platformer/App.xaml.cs
@@ -3,6 +3,7 @@ using Xamarin.Forms;
 using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Push;
 
 namespace Platformer
 {
@@ -29,6 +30,7 @@ namespace Platformer
             AppCenter.Start("android=f6bc5a20-8654-41c4-b424-3dc689594aa8;" + "uwp={Your UWP App secret here};" +
                    "ios=55099f39-256e-4196-b8eb-5873ee91e95e;",
                    typeof(Analytics), typeof(Crashes));
+            AppCenter.Start("532dbeda-61d3-4593-8cf8-de182d9af591", typeof(Push));
             base.OnStart(); 
         }
     }
