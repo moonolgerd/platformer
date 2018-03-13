f0c63790e6493ad4ea6f1bf91394f59754846fda
Oleg <moonolgerd@gmail.com>
2018-03-12 23:51:02 -0400

    Rely on AppCenter to send notifications

--- Platformer/Platformer.Android/MainActivity.cs
+++ Platformer/Platformer.Android/MainActivity.cs
@@ -7,6 +7,10 @@ using Android.Util;
 using Firebase.Messaging;
 using System;
 using Microsoft.AppCenter.Push;
+using Microsoft.AppCenter;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
 
 namespace Platformer.Droid
 {
@@ -15,7 +19,7 @@ namespace Platformer.Droid
     {
         const string TAG = "MainActivity";
 
-        protected override void OnCreate(Bundle bundle)
+        protected async override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
@@ -23,7 +27,9 @@ namespace Platformer.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+
             Push.SetSenderId("1011362886006");
+
             LoadApplication(new App());
 
             try
@@ -45,6 +51,18 @@ namespace Platformer.Droid
             }
 
             IsPlayServicesAvailable();
+
+            var isEnabled = await Push.IsEnabledAsync();
+            var id = await AppCenter.GetInstallIdAsync();
+            Log.Debug(id.ToString(), id.ToString());
+
+            //var data = JsonConvert.SerializeObject(new
+            //{
+            //    Token = id.ToString()
+            //});
+            //var client = new HttpClient();
+            //await client.PostAsync("http://10.0.2.2:3000/register",
+            //    new StringContent(data, Encoding.UTF8, "application/json"));
         }
 
         public bool IsPlayServicesAvailable()
--- Platformer/Platformer/App.xaml.cs
+++ Platformer/Platformer/App.xaml.cs
@@ -27,9 +27,9 @@ namespace Platformer
 
         protected override void OnStart()
         {
-            AppCenter.Start("android=f6bc5a20-8654-41c4-b424-3dc689594aa8;" + "uwp={Your UWP App secret here};" +
-                   "ios=55099f39-256e-4196-b8eb-5873ee91e95e;",
-                   typeof(Analytics), typeof(Crashes));
+            //AppCenter.Start("android=f6bc5a20-8654-41c4-b424-3dc689594aa8;" + "uwp={Your UWP App secret here};" +
+            //       "ios=55099f39-256e-4196-b8eb-5873ee91e95e;",
+            //       typeof(Analytics), typeof(Crashes));
             AppCenter.Start("532dbeda-61d3-4593-8cf8-de182d9af591", typeof(Push));
             base.OnStart(); 
         }
