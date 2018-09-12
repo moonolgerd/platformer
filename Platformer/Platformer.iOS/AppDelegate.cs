c500c294b64b6f3a493d6b053be4c7d4cfae50d0
Oleg <moonolgerd@gmail.com>
2018-09-12 00:41:23 -0400

    Removed unused configuration

--- Platformer/Platformer.iOS/AppDelegate.cs
+++ Platformer/Platformer.iOS/AppDelegate.cs
@@ -1,5 +1,4 @@
-﻿using Firebase.CloudMessaging;
-using Foundation;
+﻿using Foundation;
 using System;
 using UIKit;
 using UserNotifications;
@@ -7,14 +6,12 @@ using UserNotifications;
 namespace Platformer.iOS
 {
 	[Register(nameof(AppDelegate))]
-	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate, IMessagingDelegate
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
 	{
 		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
 		{
 			global::Xamarin.Forms.Forms.Init();
 			LoadApplication(new App());
-
-            Firebase.Core.App.Configure();
 
             // iOS 10 or later
             var authOptions = UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound;
@@ -22,14 +19,9 @@ namespace Platformer.iOS
                 Console.WriteLine(granted);
             });
 
-            // For iOS 10 data message (sent via FCM)
-            Messaging.SharedInstance.Delegate = this;
-
             UIApplication.SharedApplication.RegisterForRemoteNotifications();
 
             return base.FinishedLaunching(app, options);
 		}
-
-		public void DidRefreshRegistrationToken(Messaging messaging, string fcmToken) => Console.WriteLine(fcmToken);
 	}
 }
